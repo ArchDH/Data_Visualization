be59e82fac28375124e96c805274061749f845b9
DH <kdhendrix90@gmail.com>
2020-10-16 17:07:12 +0100

    No messages

---
+++ Bar_Chart_Example.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Set a Korean font
+plt.rc('font', family='chosunilboNM', size=13)
+
+# Import excel data
+raw_data = pd.read_excel('base_wage_in_Japan.xlsx', header=0)
+npdata = raw_data.to_numpy()
+
+# Get a new cmap
+cmap = plt.get_cmap('Set2', 2)
+
+# Value annotation function
+def annotate(bars, num):
+    """Attach a text label above each bar"""
+    for bar in bars:
+        height = bar.get_height()
+        ax[num].annotate('{} 만엔'.format(height),
+                         xy=(bar.get_x()+bar.get_width()/2, height), xytext=(0, 10),
+                         textcoords="offset points",
+                         ha='center', va='bottom')
+
+# Draw bar chart
+fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(8, 8))
+bars1 = ax[0].bar(npdata[:, 0], npdata[:, 1], color=cmap(0))
+ax[0].set(ylim=(0, 800))
+ax[0].set_title("나이에 따른 연봉 변화")
+ax[0].set_ylabel("연봉 (엔)")
+ax[0].set_xlabel("나이")
+annotate(bars1, 0)
+
+bars2 = ax[1].bar(npdata[:, 0], npdata[:, 2], color=cmap(1))
+ax[1].set(ylim=(0, 60))
+ax[1].set_title("나이에 따른 기본급 변화")
+ax[1].set_ylabel("기본급 (엔)")
+ax[1].set_xlabel("나이")
+annotate(bars2, 1)
+
+fig.suptitle("일본 직장인의 나이에 따른 연봉 및 기본급 평균 (2018년 일본정부)", y=1.05)
+fig.tight_layout()
+plt.savefig(fname='figure1.jpeg', facecolor=None, dpi=1200, bbox_inches='tight')
---
+++ Pie_Chart_Example.py
@@ -0,0 +1,44 @@
+# Pie chart example
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+# Change font to a Korean font
+plt.rc('font', family='chosunilboNM', size=11)
+
+# Import excel data
+data_adrs = 'sample_data.xlsx'
+# When no headers exist, you have to state header=None explicitly.
+# Pandas read method returns DataFrame
+data = pd.read_excel(data_adrs, header=None)
+# to numpy 2d array
+npdata = data.to_numpy()
+row_size = npdata.shape[1]
+
+# Draw pie charts
+# Get a new cmap
+cmap = plt.get_cmap('Set2', row_size)
+fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(14, 7))
+kw = dict(arrowprops=dict(arrowstyle="-"), zorder=0, va="center")
+sub_titles = ["국적별 외자계기업 비율", "업종별 외자계기업 비율", "지역별 외자계기업 비율"]
+
+for num in range(3):
+    wedges, texts, auto = ax[num].pie(npdata[:, 2*num-1], wedgeprops=dict(width=0.8), autopct='%1.1f%%', shadow=False,
+                                      colors=cmap(range(row_size)))
+
+    for i, p in enumerate(wedges):
+        ang = (p.theta2 - p.theta1)/2 + p.theta1
+        y = np.sin(np.deg2rad(ang))
+        x = np.cos(np.deg2rad(ang))
+        horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
+        connectionstyle = "angle,angleA=0,angleB={}".format(ang)
+        kw["arrowprops"].update({"connectionstyle": connectionstyle})
+        ax[num].annotate(npdata[i, 2*num], xy=(x, y), xytext=(1.3*np.sign(x), 1.3*y),
+                    horizontalalignment=horizontalalignment, **kw)
+
+    ax[num].set_title(sub_titles[num], y=1.01)
+
+fig.suptitle("외자계 기업의 일본지사에 대한 조사 (2018년 일본정부)", fontsize=18, y=1.02)
+plt.tight_layout()
+#plt.show()
+plt.savefig(fname='figure.jpeg', facecolor=None, dpi=1200, bbox_inches='tight')
