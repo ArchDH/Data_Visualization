70a026527149b79f725c4a2b5010a1751f8d8307
DH <kdhendrix90@gmail.com>
2020-10-21 16:28:27 +0100

    no message

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Data_Visualization
 
+1) Writer\
+ArchDH
+
+2) Purpose\
 This is a repository to store data visualization python codes.
--- test_file.py
+++ test_file.py
@@ -1,6 +1,7 @@
-import json
+from urllib.request import urlopen
 import geopandas
-import matplotlib.pyplot as plt
 
-adrs = "Geo_data/Tokyo_N03-200101_13_GML/N03-20_13_200101.json"
-tokyo = geopandas.read_file(adrs)
+url = 'https://github.com/ArchDH/Data_Visualization/blob/main/Geo_data/Tokyo_N03-200101_13_GML/N03-20_13_200101.json'
+with urlopen(url) as response:
+    tokyo = geopandas.read_file(response)
+
