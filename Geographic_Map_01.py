abbe54ef3d67bf2c07c2b80d36dc443d1a791991
DH <kdhendrix90@gmail.com>
2020-10-23 20:47:11 +0100

    no message

--- Geographic_Map_01.py
+++ Geographic_Map_01.py
@@ -1,22 +1,50 @@
-# -*- coding: utf-8 -*-
-"""
-Python Visualization
-Title : Choropleth example using GeoJason and Plotly
-Data : 20th OCT 2020
-"""
+from urllib.request import urlopen
+import geopandas as gpd
+import pandas as pd
 import matplotlib.pyplot as plt
-import geopandas
 
-# Load a geojson file
-adrs = "Geo_data/Tokyo_N03-200101_13_GML/N03-20_13_200101.json"
-tokyo = geopandas.read_file(adrs, encoding='SHIFT-JIS')
+# Get Korean font
+plt.rc('font', family='chosunilboNM', size=13)
 
-# Plotting
-ax = plt.axes()
-ax.set(xlim=(138.8, 140), ylim=(35.4, 36))
-# polygon infill
-tokyo.plot(ax=ax)
-# boundary
-tokyo.boundary.plot(ax=ax, edgecolor='k')
+# The urls must be given as a raw file
+# Obtain geojson data
+url_json = 'https://raw.githubusercontent.com/ArchDH/Data_Visualization/main/Geo_data/Tokyo_N03-200101_13_GML/N03-20_13_200101.geojson'
+with urlopen(url_json) as response:
+    tokyo = gpd.read_file(response, encoding='SHIFT-JIS')
+tokyo = tokyo[:113]
+
+# Obtain average rent data
+url_rent = 'https://raw.githubusercontent.com/ArchDH/Data_Visualization/main/excel_data/average_rent_in_Tokyo_2020.xlsx'
+with urlopen(url_rent) as response:
+    avr_rent = pd.read_excel(response, ecoding='SHIFT-JIS')
+rent = [0]*tokyo.shape[0]
+for i, ward in enumerate(tokyo['N03_004'][:113]):
+    rent[i] = (avr_rent.loc[avr_rent['district'] == ward]['1K/1DK'].to_numpy()[0][:3])
+tokyo['avr_rent'] = rent
+
+# Obtain centroid of each polygon
+gs = gpd.GeoSeries(tokyo['geometry'])
+crs = gs.to_crs(epsg=6668)
+tokyo['centroid'] = crs.centroid
+
+# Plot geojson data
+# Geopandas mapping tool is the interface to the matplotlib library.
+plt.style.use('ggplot')
+fig, ax = plt.subplots(figsize=(10, 7))
+tokyo.plot(column='avr_rent', legend=True, legend_kwds={"title": "월세 [만엔]", "bbox_to_anchor": (1.3, 1.0), "frameon": False}
+           , edgecolor='grey', cmap=plt.cm.Greens, ax=ax)
+ax.set(xlim=(139.5, 139.934), ylim=(35.4846, 35.9097))
+
+# Ward name annotation
+# googletrans library is the google translator API
+from googletrans import Translator
+translator = Translator()
+for (i, ward), cent in zip(enumerate(tokyo[:-1]['N03_004']), tokyo['centroid']):
+    if ward != tokyo['N03_004'].at[i+1]:
+        kr_ward = translator.translate(ward, dest='korean')
+        ax.annotate(kr_ward.text, xy=(cent.x, cent.y))
+
+plt.title("도쿄 23구내 월세평균", size=16)
 plt.tight_layout()
 plt.show()
+# #plt.savefig(fname='figure.jpeg', facecolor=None, dpi=1200, bbox_inches='tight')
--- test_file.py
+++ test_file.py
@@ -1,7 +1,17 @@
 from urllib.request import urlopen
-import geopandas
+import geopandas as gpd
+import pandas as pd
+import matplotlib.pyplot as plt
+import googletrans as gt
 
-url = 'https://github.com/ArchDH/Data_Visualization/blob/main/Geo_data/Tokyo_N03-200101_13_GML/N03-20_13_200101.json'
+# Get Korean font
+plt.rc('font', family='chosunilboNM', size=13)
+
+# The url must be given as a raw file
+url = 'https://raw.githubusercontent.com/ArchDH/Data_Visualization/main/Geo_data/Tokyo_N03-200101_13_GML/N03-20_13_200101.geojson'
 with urlopen(url) as response:
-    tokyo = geopandas.read_file(response)
+    tokyo = gpd.read_file(response, encoding='SHIFT-JIS')
 
+for i, v in enumerate(tokyo[:-1]['N03_004']):
+    if v != tokyo['N03_004'].at[i+1]:
+        print(v)
